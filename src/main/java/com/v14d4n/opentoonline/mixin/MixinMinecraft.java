0957763678732a8910064a5d8df228aef3430719
Vladislav "v14d4n" Nefedov <vlad_nefedov_2002@mail.ru>
2022-08-17 23:02:10 +0400

    fix localization for window title

--- src/main/java/com/v14d4n/opentoonline/mixin/MixinMinecraft.java
+++ src/main/java/com/v14d4n/opentoonline/mixin/MixinMinecraft.java
@@ -3,18 +3,20 @@ package com.v14d4n.opentoonline.mixin;
 import com.v14d4n.opentoonline.network.UPnPHandler;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.SharedConstants;
+import net.minecraft.util.text.TranslationTextComponent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Minecraft.class)
-public class MixinMinecraft {
+public abstract class MixinMinecraft {
 
     @Inject(method = "createTitle()Ljava/lang/String;", at = @At("HEAD"), cancellable = true)
     private void createTitle(CallbackInfoReturnable<String> cir) {
         if (UPnPHandler.getClosePortAfterLogout()) {
-            String title = "Minecraft* " + SharedConstants.getCurrentVersion().getName() + " - Multiplayer (Open2Online)";
+            String localizedMultiplayer = new TranslationTextComponent("title.opentoonline.multiplayer").getString();
+            String title = "Minecraft* " + SharedConstants.getCurrentVersion().getName() + " - " + localizedMultiplayer + " (Open2Online)";
             cir.setReturnValue(title);
         }
     }
